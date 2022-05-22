99b50e792a5c800b18d502a2af23ada99da0d7cc
mgarcia <maximo.garcia@uma.es>
2022-05-22 17:04:04 +0200

    Timer = StopWatch

---
+++ MoneySaving/src/StopWatch.java
@@ -0,0 +1,23 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class StopWatch {
+    Timer timer;
+
+    public StopWatch(int minutes) {
+        timer = new Timer();
+        timer.schedule(new StopTask(), minutes * 60 * 1000);
+    }
+
+    public static void main(String[] args) {
+        new StopWatch(1);
+        System.out.println("El tiempo comienza");
+    }
+
+    class StopTask extends TimerTask {
+        public void run() {
+            System.out.println("Time Up!");
+            timer.cancel();
+        }
+    }
+}
